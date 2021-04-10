50551f55c09bf8923ce5ac76d77f91368c3c021c
AkashSDas <aakashdas368@gmail.com>
2021-04-10 19:59:37 +0530

    rm methods from DB class and refactor DATABASE_NAME

---
+++ src/main/java/database/DB.java
@@ -0,0 +1,19 @@
+package database;
+
+public class DB {
+    // JDBC driver name and database URL
+    static final String DATABASE_NAME = "property_hub";
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/" + DATABASE_NAME;
+
+    // Database credentials
+    static final String USER = "dbadmin";
+    static final String PASSWORD = "password";
+
+    /**
+     * To avoid the below warning, adding the private empty constructor Add a
+     * private constructor to hide the implicit public one.sonarlint(java:S1118)
+     */
+    private DB() {
+    }
+}
