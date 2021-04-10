bc242d29538430b911ca7b7626afbd6808baaabb
AkashSDas <aakashdas368@gmail.com>
2021-04-10 20:08:13 +0530

    create Home servlet and implement doGet method

---
+++ src/main/java/servlets/Home.java
@@ -0,0 +1,35 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.LinkedList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import database.SearchPropertyDB;
+import models.Property;
+
+@WebServlet("/home")
+public class Home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public Home() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		LinkedList<Property> properties = SearchPropertyDB.getAllProperties();
+		request.setAttribute("properties", properties);
+		String jspFileUrl = "/views/HomePage.jsp";
+		getServletContext().getRequestDispatcher(jspFileUrl).forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
